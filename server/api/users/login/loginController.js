9183b8284b939c4ee8487a5b3fbf9b9350336877
brettcelestre <bcelestre@gmail.com>
2018-01-09 10:51:49 -0800

    Updates login to work properly

--- server/api/users/login/loginController.js
+++ server/api/users/login/loginController.js
@@ -3,6 +3,9 @@
 // Stubs
 const loginStubs = require('./loginStubs');
 
+// Models
+const User = require('../signUp/signUpModel');
+
 module.exports = {
 
   login: function(req, res) {
@@ -11,15 +14,37 @@ module.exports = {
       res.status(200).send(loginStubs.post(req.headers['x-stub']));
     }
 
-    console.log('loginController login() - req.body.username: ', req.body.username);
-    console.log('loginController login() - req.body.password: ', req.body.password);
-    req.session.user = req.body.username;
-    console.log('req.session = ', req.session);
-    
-    // Check users password
-      // if password is correct, start a new session
-        res.status(200).send({username: req.body.username});
-      // if incorrect, throw error
-    
+    const username = req.body.username,
+          plainPassword = req.body.password;
+
+    // Searches database for this user
+    User.findOne({username: username})
+      .exec(function(err, user) {
+          if (!user) {
+            res.status(404).send({error: 'User not found'});
+          } else {
+            // Checks if password matches encrypted password in database
+            return user.comparePasswords(plainPassword)
+            .then(function(foundUser) {
+              if (!foundUser) {
+                res.status(404).send({error: 'password does not match'});
+              } else {
+                // Regenerates new session
+                req.session.regenerate(function() {
+                  // Sets session to this user
+                  req.session.user = user;
+                  console.log('req.session = ', req.session);
+                  // Sends back only username and symbol
+                  res.send({
+                    username: user.username,
+                    words: user.words,
+                    createdAt: user.createdAt
+                  });
+                });
+              }
+            });
+          }
+      });
   }
+
 };
