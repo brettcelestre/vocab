54ddcdc90b69bad5eb35b5fe7373f152f697f535
brettcelestre <bcelestre@gmail.com>
2018-02-15 15:35:11 -0800

    Functioning getword call

--- server/backend/helpers.js
+++ server/backend/helpers.js
@@ -1,11 +1,39 @@
+
 'use strict';
 
-const oxford = require('./oxford').api;
+const db = require('./db');
+const oxford = require('./oxford');
 
-const helpers = {};
+const getWord = (word) => {
+  console.log('Helpers > getWord =', word);
 
-helpers.getWord = (word) => {
-  return oxford.api.getDefinition(word);
+  // Check DB
+  return db.api.getWord(word)
+    .then((data) => {
+      if (data === null) {
+        console.log('Helpers - word does not exist');
+        // Get word from Oxford API
+        oxford.api.getDefinition(word)
+          .then((data) => {
+            console.log(' >>> got word from oxford = ', data);
+            // Then save word in our DB
+            db.api.saveWord(data);
+            return data;
+          });
+      } else {
+        console.log('Helpers - We already have the word');
+        return data;
+      }
+    })
+    .catch((err) => {
+      throw err;
+    });
 };
 
-module.exports = helpers;
+// helpers.addWord = (word) => {
+//   return oxford.api.getDefinition(word);
+// };
+
+module.exports = {
+  getWord
+};
